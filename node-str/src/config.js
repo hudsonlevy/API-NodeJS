ccc3b6d5009ba20610e772554bea7112624ca5ae
hudson <hudson@C3-LAB-PESQ-GRE.IASD.ORG>
2018-06-05 22:23:53 -0300

    videoaula 31 e 32 NodeJS

--- node-str/src/config.js
+++ node-str/src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store';
 
 module.exports = {
     connectionString: 'mongodb://hudson:501352@ds014118.mlab.com:14118/nodestr',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
